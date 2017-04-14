e8042d4c01f138421af0d9771c1114656b452f6b
Muhammad Naufal <mnaufal75@gmail.com>
2017-04-14 19:26:44 +0700

    Add Validation Login

--- routes/index.js
+++ routes/index.js
@@ -11,7 +11,7 @@ router.get('/', function(req, res, next) {
 router.post('/', function(req, res, next) {
 	var url = "https://dev.prelo.id/api/auth/login";
 
-  var body = {
+  var bodies = {
     username_or_email: req.body.username_or_email,
     password: req.body.password
   }
@@ -19,10 +19,12 @@ router.post('/', function(req, res, next) {
   request({
     method: 'post',
     url: url,
-    body: body,
+    body: bodies,
     json: true
   }, function (error, response, body) {
-    if (!error && response.statusCode === 200) {
+    if (bodies.username_or_email.length < 4 || bodies.password.length < 6) {
+    	res.send("username kurang dari 4 karakter atau password kurang dari 6 karakter");
+    } else if (!error && response.statusCode === 200) {
     	//app.locals.username = 'asdasd';
     	app.locals.token = body._data.token;
     	token = body._data.token;
