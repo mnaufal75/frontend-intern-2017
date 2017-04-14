11fb4ddfcd44a70a7fb618d91c18f1393adb6bbb
Muhammad Naufal <mnaufal75@gmail.com>
2017-04-14 14:19:30 +0700

    Connect to API

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,37 @@
 var express = require('express');
+var request = require('request');
 var router = express.Router();
+var app = express();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+router.post('/', function(req, res, next) {
+	var url = "https://dev.prelo.id/api/auth/login";
+
+  var body = {
+    username_or_email: req.body.username_or_email,
+    password: req.body.password
+  }
+
+  request({
+    method: 'post',
+    url: url,
+    body: body,
+    json: true
+  }, function (error, response, body) {
+    if (!error && response.statusCode === 200) {
+    	//app.locals.username = 'asdasd';
+    	app.locals.token = body._data.token;
+    	token = body._data.token;
+    	console.log(token);
+      res.redirect('/users');
+    } else {
+      next(error);
+    }
+  })
+})
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,36 @@
 var express = require('express');
+var request = require('request');
 var router = express.Router();
+var app = express();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  var url = "https://dev.prelo.id/api/me/lovelist"
+  
+  var header = {
+    Authorization: "Token " + "63ed74bddec13ea0ac1a72d5f676208450604c9d"
+  }
+
+  console.log(header.Authorization);
+
+  request({
+    method: 'get',
+    url: url,
+    headers: header,
+    json: true
+  }, function (error, response, body) {
+    if (!error && response.statusCode === 200) {
+      res.render('lovelist', {
+        data: body._data
+      })
+    } else {
+      next(error);
+    }
+  })
+});
+
+router.post('/', function(req, res, next) {
+  
 });
 
 module.exports = router;
