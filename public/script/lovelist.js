65c91bc2b3cd910faec4ab45662967c582d64454
Muhammad Naufal <mnaufal75@gmail.com>
2017-04-14 23:41:55 +0700

    Change money format

--- public/script/lovelist.js
+++ public/script/lovelist.js
@@ -2,8 +2,16 @@ function toComma(x) {
     return "Rp " + x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
 }
 
-$(document).ready(function () {
-	$('.harga-barang').each(function() {
-		toComma( $(".harga-barang") );
-	});
-});
+// $(document).ready(function () {
+// 	$('.harga-barang').each(function(index) {
+// 		console.log( $(".harga-barang")[index].innerHTML );
+// 		$(".harga-barang")[index].html(toComma( $(".harga-barang")[index].innerHTML ));
+// 	});
+// });
+
+var hargas = document.getElementsByClassName("harga-barang");
+
+for(var i = 0; i < hargas.length; i++) {
+	var harga = hargas[i];
+	harga.innerHTML = toComma(harga.innerHTML);
+}
