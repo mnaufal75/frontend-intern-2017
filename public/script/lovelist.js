f9e75563d999264b37753803eabc87579c071abb
Muhammad Naufal <mnaufal75@gmail.com>
2017-04-14 23:57:48 +0700

    Centering and cleaning

--- public/script/lovelist.js
+++ public/script/lovelist.js
@@ -2,13 +2,6 @@ function toComma(x) {
     return "Rp " + x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
 }
 
-// $(document).ready(function () {
-// 	$('.harga-barang').each(function(index) {
-// 		console.log( $(".harga-barang")[index].innerHTML );
-// 		$(".harga-barang")[index].html(toComma( $(".harga-barang")[index].innerHTML ));
-// 	});
-// });
-
 var hargas = document.getElementsByClassName("harga-barang");
 
 for(var i = 0; i < hargas.length; i++) {
